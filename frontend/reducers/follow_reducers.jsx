0765e41edea1d299eeac86cca47375b9bf7e8874
andrew1007 <yueh.andrew@gmail.com>
2017-04-19 20:02:40 -0700

    adjusted login modal

--- frontend/reducers/follow_reducers.jsx
+++ frontend/reducers/follow_reducers.jsx
@@ -9,14 +9,11 @@ const FollowReducer = (state = defaultState, action) =>{
   Object.freeze(state);
   switch(action.type){
     case RECEIVE_FOLLOW:
-      //////console.log("receieved follow");
 
       return 0
     case DELETE_FOLLOW:
-      //////console.log("deleted follow");
       return 0
     default:
-      //////console.log('no follow actions hit');
       return state
     }
 }
