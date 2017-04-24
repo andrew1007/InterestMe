ecf2dc9bc4d0c1d2f08051e38c30696bed7b20f5
andrew1007 <yueh.andrew@gmail.com>
2017-04-24 09:21:54 -0700

    heroku push

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -27,22 +27,6 @@ helper_method :current_user
     render :show
   end
 
-  describe 'POST #create' do
-    context 'when password invalid' do
-      it 'renders error JSON' do
-        debugger
-        payload = {user: {
-          username: "andrew",
-          password: "ds",
-          description: ""
-        }}
-        expect(response.status).to eq(422)
-        error = @user.errors.full_messages
-        expect(error).to eq(["Password is too short (minimum is 6 characters)"])
-      end
-    end
-
-  end
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
