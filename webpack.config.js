95df3b935773b0959c4f23968ba6a54c0d23d586
andrew1007 <yueh.andrew@gmail.com>
2017-07-27 16:57:26 -0700

    production mode

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,11 @@ module.exports= {
     extensions: ['', '.js', '.jsx' ]
   },
 };
-// plugins: [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }),
-//   new webpack.optimize.UglifyJsPlugin()
-// ]
+plugins: [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin()
+]
