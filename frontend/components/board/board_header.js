f526ce9e159013e4bec57df47f8bffe8e7abed23
andrew1007 <yueh.andrew@gmail.com>
2017-09-12 15:37:12 -0700

    change board header into arrow function

--- frontend/components/board/board_header.js
+++ frontend/components/board/board_header.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class BoardHeader extends Component {
-  render() {
-    return(
-      <div className='board-header-container'>
-        <div className='board-header-underbar'>
-          <div className='board-header-name'>
-            {this.props.name}
-          </div>
-          <div className='board-header-username'>
-            {this.props.username ? `A board by ${this.props.username}` : null}
-          </div>
-        </div>
+const BoardHeader = props => (
+  <div className='board-header-container'>
+    <div className='board-header-underbar'>
+      <div className='board-header-name'>
+        {props.name}
       </div>
-    )
-  }
-}
+      <div className='board-header-username'>
+        {props.username ? `A board by ${props.username}` : null}
+      </div>
+    </div>
+  </div>
+)
+
+export default BoardHeader
